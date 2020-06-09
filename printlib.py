d02205919ef761c427c534528a16c26d02016f0b
ppcrong <ppcrong@gmail.com>
2020-06-09 16:21:41 +0800

    [printlib] add get_caller_info api

--- printlib.py
+++ printlib.py
@@ -22,9 +22,34 @@ class printlib:
 
         # Get time
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.%f')[:-3]
+
         if the_class:
             print(
                 "{} [{}.{}#{}] {}".format(timestamp, the_class, the_method, the_line_number, " ".join(map(str, args))),
                 **kwargs)
         else:
             print("{} [{}#{}] {}".format(timestamp, the_method, the_line_number, " ".join(map(str, args))), **kwargs)
+
+    @staticmethod
+    def get_caller_info(level=1):
+        stack = inspect.stack()
+
+        # Get class name
+        try:
+            the_class = stack[level][0].f_locals["self"].__class__.__name__
+        except KeyError as e:
+            the_class = None
+
+        # Get function name
+        the_method = stack[level][0].f_code.co_name
+
+        # Get line number
+        the_line_number = stack[level][0].f_lineno
+
+        # Get time
+        timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.%f')[:-3]
+
+        if the_class:
+            return "{} [{}.{}#{}]".format(timestamp, the_class, the_method, the_line_number)
+        else:
+            return "{} [{}#{}]".format(timestamp, the_method, the_line_number)
