849135123b5ad090d4002817677d104f83ff480c
ppcrong <ppcrong@gmail.com>
2020-08-21 18:13:47 +0800

    [serlib] add buffer read/write, thread read/write, timeout read/write, data read/write

--- serlib.py
+++ serlib.py
@@ -1,3 +1,6 @@
+import queue
+from concurrent.futures.thread import ThreadPoolExecutor
+
 import serial
 
 from loglib import loglib
@@ -13,8 +16,10 @@ class serlib:
     def __init__(self, port: str, baudrate: int = 115200):
         self.port = port
         self.serial = None
+        self.bufr = queue.Queue()
+        self.bufw = queue.Queue()
         try:
-            self.serial = serial.Serial(port=port, baudrate=baudrate)
+            self.serial = serial.Serial(port=port, baudrate=baudrate, timeout=1, write_timeout=1)
         except serial.serialutil.SerialException as e:
             self.logger.error(e)
 
@@ -25,11 +30,73 @@ class serlib:
             self.logger.error('serial is None!!!')
             return False
 
+    def in_waiting(self):
+        if self.serial:
+            return self.serial.inWaiting()
+        else:
+            self.logger.error('serial is None!!!')
+            return False
+
     def close(self):
         if self.serial:
             self.serial.close()
         else:
             self.logger.error('serial is None!!!')
+
+    def write_data(self, data: str):
+        self.bufw.put(data)
+
+    def write(self, data: str):
+        if self.serial:
+            try:
+                len_write = self.serial.write(data.encode())
+                self.logger.info(f'WRITE len({len_write})')
+                self.logger.info(f'WRITE >>> {data}')
+            except serial.serialutil.SerialException as e:
+                self.logger.error(e)
+            except serial.serialutil.SerialTimeoutException as e:
+                self.logger.error(e)
+        else:
+            self.logger.error('serial is None!!!')
+
+    def read(self, size: int):
+        ret = None
+        if self.serial:
+            try:
+                raw = self.serial.read(size=size)
+                data = raw.decode()
+                self.logger.info(f'READ len({len(data)})')
+                self.logger.info(f'READ <<<\n{data}')
+                ret = data
+            except serial.serialutil.SerialException as e:
+                self.logger.error(e)
+        else:
+            self.logger.error('serial is None!!!')
+        return ret
+
+    def readline(self):
+        ret = None
+        if self.serial:
+            try:
+                raw = self.serial.readline()
+                data = raw.decode()
+                self.logger.info(f'READ len({len(data)})')
+                self.logger.info(f'READ <<< {data}')
+                ret = data
+            except serial.serialutil.SerialException as e:
+                self.logger.error(e)
+        else:
+            self.logger.error('serial is None!!!')
+        return ret
+
+    def start(self):
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            executor.submit(self.write_thread)
+            executor.submit(self.read_thread)
+        self.logger.info('all done!!!')
+
+    def stop(self):
+        self.close()
 
     # region [with]
     def __enter__(self):
@@ -39,6 +106,41 @@ class serlib:
         self.close()
 
     # endregion [with]
+
+    # region [thread]
+    def read_thread(self):
+        """
+        keep reading and put data into read buffer
+        """
+        if not self.serial:
+            self.logger.error('serial is None!!!')
+            return
+
+        while self.is_opened():
+            size = self.in_waiting()
+            # self.logger.info(f'size: {size}')
+            if size:
+                data = self.read(size)
+                if data:
+                    self.bufr.put(data)
+
+        self.logger.info('EXIT reading...')
+
+    def write_thread(self):
+        """
+        keep writing when write buffer has data
+        """
+        if not self.serial:
+            self.logger.error('serial is None!!!')
+            return
+
+        while self.is_opened():
+            if not self.bufw.empty():
+                self.write(self.bufw.get(block=True, timeout=1))
+
+        self.logger.info('EXIT writing...')
+
+    # endregion [thread]
 
     @staticmethod
     def comports():
