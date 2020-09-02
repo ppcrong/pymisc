682d45900fdf1ec90443fe1f8bbbe3f12497dd8e
ppcrong <ppcrong@gmail.com>
2020-09-02 15:52:12 +0800

    [vslib] support video file

--- vslib.py
+++ vslib.py
@@ -15,15 +15,18 @@ class vslib:
     thread = None
     logger = loglib(__name__)
 
-    def __init__(self, src: int = 0, width: int = 640, height: int = 480):
+    def __init__(self, src=0, width: int = 0, height: int = 0):
         self.src = src
         self.stream = cv2.VideoCapture(src)
-        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        if width and height:
+            self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+            self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.logger.info(f'(w, h, fps, fcounts): {self.getinfo()}')
         (self.grabbed, self.frame) = self.stream.read()
         self.started = False
         self.read_lock = Lock()
 
+    # region [camera]
     def start(self):
         if self.started:
             self.logger.warning('already started!!!')
@@ -55,6 +58,21 @@ class vslib:
         if self.thread and self.thread.is_alive():
             self.thread.join()
 
+    # endregion [camera]
+
+    # region [video]
+    def _read(self):
+        """
+        directly read for video case
+        """
+        (self.grabbed, self.frame) = self.stream.read()
+        return self.frame
+
+    # endregion [video]
+
+    def set(self, propid: int, value: int):
+        self.stream.set(propId=propid, value=value)
+
     def is_opened(self):
         return self.stream.isOpened()
 
@@ -65,14 +83,16 @@ class vslib:
         w = self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)
         h = self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)
         fps = self.stream.get(cv2.CAP_PROP_FPS)
-        return w, h, fps
+        fcounts = self.stream.get(cv2.CAP_PROP_FRAME_COUNT)
+        return w, h, fps, fcounts
 
     # region [with]
     def __enter__(self):
+        self.logger.info()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.logger.info('release')
+        self.logger.info()
         self.release()
     # endregion [with]
 
@@ -80,20 +100,21 @@ class vslib:
 if __name__ == "__main__":
     import datetime
 
+    # camera
     with vslib() as vs:
         if not vs.is_opened():
             print(f'open source {vs.src} fail!!!')
         else:
             vs.start()
-            w, h, fps = vs.getinfo()
-            print(f'w: {w}, h: {h}, fps: {fps}')
-            print()
             while True:
+                # get frame (calculate time diff)
                 time_start = datetime.datetime.now()
                 frame = vs.read()
                 time_end = datetime.datetime.now()
-                print(f'time: {(time_end - time_start).total_seconds() * 1000 : 0.3f} ms')
+                print(f'vs.read() time: {(time_end - time_start).total_seconds() * 1000 : 0.3f} ms')
+                # display
                 cv2.imshow('webcam', frame)
+                # wait for ESC key
                 if cv2.waitKey(1) == 27:
                     break
 
@@ -113,4 +134,32 @@ if __name__ == "__main__":
             print(f'fps: {fps : 0.3f}')
 
             vs.stop()
+            cv2.destroyAllWindows()
+
+    # video
+    with vslib('d:/_PROJECT/KL720/Kobe/yolov3_416.mp4') as vs:
+        if not vs.is_opened():
+            print(f'open source {vs.src} fail!!!')
+        else:
+            _, _, fps, fcounts = vs.getinfo()
+            delay = int(1000 / fps)
+            print(f'delay: {delay} ms')
+            print(f'video duration: {delay * fcounts} ms')
+            while vs.grabbed:
+                # NOTE!!!set pos_frames will take around 70~140ms
+                # set frame position
+                # vs.set(cv2.CAP_PROP_POS_FRAMES, i)
+
+                # get frame (calculate time diff)
+                time_start = datetime.datetime.now()
+                frame = vs._read()
+                time_end = datetime.datetime.now()
+                print(f'vs._read() time: {(time_end - time_start).total_seconds() * 1000 : 0.3f} ms')
+                if not vs.grabbed:
+                    break
+                # display
+                cv2.imshow('video', frame)
+                # wait for ESC key
+                if cv2.waitKey(delay) == 27:
+                    break
             cv2.destroyAllWindows()
